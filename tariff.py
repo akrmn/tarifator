acbf9d5ad32fa88054b1c6f21aeec20090d084a8
akrmn <unacson@gmail.com>
2015-01-17 23:45:33 -0430

    cleanup

--- tariff.py
+++ tariff.py
@@ -27,7 +27,6 @@ class Tariff:
             ) >
             60*60*1000000
         )
-        # assert(nightStart - dayStart > timedelta(hours = 1))
         self.dayRate = dayRate
         self.nightRate = nightRate
         self.dayStart = dayStart
@@ -56,40 +55,3 @@ class Tariff:
                 total = total + self.nightRate
             current = current + ONE_HOUR
         return(total)
-
-
-
-    # def calcularMonto(self,fi,ff):
-    #     reserva = ff-fi
-
-    #     # Assert para verificar que las fechas son validas
-    #     assert(259200 >= reserva.days*24*60*60 + reserva.seconds >= 900)
-
-    #     totalHoras = reserva.days*24+reserva.seconds/3600
-    #     minutos = (reserva.seconds%3600)/60
-    #     print(reserva.days, reserva.seconds)
-    #     horasNoche = 0
-    #     horasDia   = 0
-    #     while(totalHoras > 0):
-    #         if(fi.hour >= 6 and fi.hour < 18):
-    #             diferencia = 18 - fi.hour if 18 - fi.hour < totalHoras else totalHoras
-    #             horasDia += diferencia
-    #         elif (fi.hour >= 18):
-    #             diferencia = 24 - fi.hour if 24 - fi.hour < totalHoras else totalHoras
-    #             horasNoche += diferencia
-    #         else:
-    #             diferencia = 6 - fi.hour if 6 - fi.hour < totalHoras else totalHoras
-    #             horasNoche += diferencia
-
-    #         totalHoras -= diferencia
-    #         fi = fi + timedelta(hours = (diferencia))
-
-    #     monto = horasDia*self.tarifaDia + horasNoche*self.tarifaNoche
-    #     if((fi.hour == 5 and fi.minute + minutos > 59) or
-    #         (fi.hour == 17 and fi.minute + minutos > 59)):
-    #         monto += self.tarifaDia if self.tarifaDia > self.tarifaNoche else self.tarifaNoche
-    #     elif(fi.minute + minutos > 0) :
-    #          monto += self.tarifaDia if self.tarifaDia < self.tarifaNoche else self.tarifaNoche
-    #     print(horasDia,"dia")
-    #     print(horasNoche,"noche")
-    #     return monto
