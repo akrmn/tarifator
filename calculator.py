65439d5c306c1392bcfa0e9a637e059406ebc552
akrmn <unacson@gmail.com>
2015-01-20 19:33:50 -0430

    finished main program

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from decimal import *
+from datetime import datetime
 from tariff import Tariff
 TWOPLACES = Decimal(10) ** -2
 
@@ -20,6 +21,26 @@ def getTariff():
         done = input("Are those rates OK? (y/n) > ")
     return(t)
 
+def getDatetime(moment):
+    print("{0} date-time:".format(moment))
+    year = int(input("\tenter year (yyyy) > "))
+    month = int(input("\tenter month (mm) > "))
+    day = int(input("\tenter day (dd) > "))
+    hour = int(input("\tenter hour (hh) > "))
+    minute = int(input("\tenter minute (mm) > "))
+    return(
+        datetime(
+            year = year,
+            month = month,
+            day = day,
+            hour = hour,
+            minute = minute
+        )
+    )
+
 if __name__ == '__main__':
     t = getTariff()
     while True:
+        startDatetime = getDatetime("Start")
+        endDatetime = getDatetime("End")
+        print(t.charge(startDatetime, endDatetime))
